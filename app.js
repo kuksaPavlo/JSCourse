78b706b52b00dc60b59c65171c644c5610a4485b
Perenio1 <perenio1@MBP-2372.perenio.net>
2021-10-27 18:26:02 +0300

    variables

---
+++ app.js
@@ -0,0 +1,142 @@
+ //alert('Hello world');
+//    console.log(2022, 1.2);
+//    console.log(20 * 'asdsasd');
+//    console.log(1 / 0);
+//    console.log('hhdhdh', "jjdjdjd", `jjdjdjd`);
+//    console.log(true,false);
+//    console.log(null);
+//    console.log(undefined);
+//    console.log(Symbol());
+//    console.log({ name: 'Denis', age: 30,  });
+//    console.table({ name: 'Denis', age: 30,  });
+
+//    console.log([1,2,3,4]);
+// // // // // // // // // 
+//var, let, const
+// var name = 'Denis';
+// console.log(name);
+// name = 'Den';
+// console.log(name);
+
+// var age;
+// console.log(age);
+// age = 40;
+// console.log(age);
+
+// var $name = 'Ivan';
+// var _name = 'Ivan';
+// // var 1sdf = '';
+// var userAge = 30;
+// var user_age = 30;
+
+// console.log(car);
+// var car = 'bnm';
+// var car = 'ddd';
+// console.log(car);
+
+// //let
+// console.clear();
+
+// //console.log(nickName);
+// let nickName = 'dmgame';
+// nickName = 'dmgamess';
+// console.log(nickName);
+
+// //const
+// // const fname = 'rrrr';
+// // fname = 'dddd';
+// // console.log(fname);
+
+// const user = {
+//   nam: 'Denis',
+//   ag: 3
+// };
+// user.ag = 22;
+// console.log(user);
+// //////////////////
+
+// let value;
+
+// //Number to string
+// value = String(10);//string
+// value = String(10+3);//string
+// value = (40).toString();//string
+
+// //Boolean to String
+// value = String(true);//string
+
+// //Array to string
+// value = String([1,2,3,4]);//string
+// //Object to string
+// value = String({nam: 'Denis'});//string [object Object]
+
+// value = 30 + '' + 30;//string 3030
+// value = 30 - 'cvfd';//number NaN
+// value = 30 * '2';//number 60
+
+// value = true + 10;//number 11
+// value = false * 2;//number 0
+// value = true + undefined;//number NaN
+
+// //string to number
+// value = Number('23');//number 23
+// value = Number(true);//number 1
+// value = Number(null);//number 0
+// value = Number('false');//number NaN
+// value = Number([1,23,4]);//number NaN
+
+// value = parseInt('200sdfg');//number 200
+// value = parseInt('aaaa200sdfg');//number NaN
+// value = parseFloat('200.212px');//number 200.212
+
+// //Boolean
+// value = Boolean('Hello');//boolean true
+// value = Boolean('');//boolean false
+// value = Boolean(100);//boolean true
+// value = Boolean(-222);//boolean true
+// value = Boolean(0);//boolean false
+// value = Boolean(undefined);//boolean false
+// value = Boolean({});//boolean true
+// value = Boolean([]);//boolean true
+// value = Boolean(null);//boolean false
+
+// let value = "hello";
+// value = 123;
+// value = 123.45;
+// alert(1/0);
+// alert(Infinity);
+// alert("sdf" * 1 + 4);
+// const bigInt = 99999999999999999999999999999999999999999n;
+
+// let str = "Hello";
+// let str2 = 'dfg';
+// let phrase = `dfghjhgfd`
+// console.log(str,str2,phrase);
+
+//  console.log(bigInt);
+//  console.log(typeof bigInt);
+
+
+// let myName = "Pavlo"
+// alert(`Hello ${myName}`);
+// alert(`Result ${3*7}`);
+
+// let isGreater = 4 > 6;
+// alert(isGreater);
+
+// let age = null;
+// alert(age);
+
+// let age = 123;
+// age = undefined;
+// alert(age);
+
+typeof (undefined);//undefined
+typeof 0;//number
+typeof 10n;//bigInt
+typeof true;//boolean
+typeof "foo";//string
+typeof Symbol("id");//Symbol
+typeof Math;//Object
+typeof null;//Object
+typeof alert;//function
